75590607b4d2df82b0de40b1de51e78af080aa9a
harshch1803 <harshch1803@gmail.com>
2016-01-18 19:20:52 +0530

    Update Destroy.cs

--- TrickOrTreat/Destroy.cs
+++ TrickOrTreat/Destroy.cs
@@ -5,6 +5,7 @@ public class Destroy : MonoBehaviour
 {
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		//destroys candies,skull,spider on collision with death zone.
 		Destroy(other.gameObject);
 	}
 
