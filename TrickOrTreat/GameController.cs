42049121ef9a7e74ec6c65f14bb37cf9a0c35761
harshch1803 <harshch1803@gmail.com>
2016-01-18 19:34:59 +0530

    Update GameController.cs

--- TrickOrTreat/GameController.cs
+++ TrickOrTreat/GameController.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
 public class GameController : MonoBehaviour 
 {
 	public Camera cam;
 	float maxwidth;
+	
+	//stores different colored candies.
 	public GameObject[] candies;
-	public GameObject restart,MainMenu;
+	
+	public GameObject restart;
+	public GameObject MainMenu;
 	public Text instruction;
 	
 	HighScore highscore;
@@ -14,6 +19,7 @@ public class GameController : MonoBehaviour
 	
 	void Awake()
 	{
+		//disable MainMenu and restart buttons.
 		MainMenu.SetActive(false);
 		restart.SetActive(false);
 	}
@@ -21,43 +27,66 @@ public class GameController : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
+		//checks for camera and assigns main camera if no camera assigned earlier.
 		if(cam==null)
 			cam=Camera.main;
-			
+		
+		//acess TimeCounter script.	
 		timecounter = gameObject.GetComponent<TimeCounter>();
 		
-		highscore = GameObject.Find("GameController").GetComponent<HighScore>();
+		//acess HighScore Script.
+		highscore = gameObject.GetComponent<HighScore>();
 		
+		//stores the screen limits in a vector.
 		Vector3 screenlimit = new Vector3(Screen.width,Screen.height,0f);
-		Vector3 worldlimit = cam.ScreenToWorldPoint(screenlimit);
-		maxwidth = worldlimit.x-.9f;
 		
+		//converts screen limits to world limits and stores it in a vector.
+		Vector3 worldlimit = cam.ScreenToWorldPoint(screenlimit);
+		
+		//(-0.9f),so that entire pumpkin stays inside the screenlimit.
+		maxwidth = worldlimit.x - 0.9f;
+		
+		//display instructions before game starts.
 		instruction.enabled=true;
 		
+		//begin spawning candies and other GOs. 
 		StartCoroutine("spawn");
 	}
 
 	IEnumerator spawn()
 	{
+		//wait for 3 seconds.
 		yield return new WaitForSeconds(3f);
 		
+		//disable instructions.
 		instruction.enabled=false;
+		
+		//begin the countdown timer.
 		timecounter.playing=true;
 		
+		//begin spawning candies after 2seconds.
 		yield return new WaitForSeconds(2f);
 		
+		//check if counter reached zero.
 		while(timecounter.playing)
 		{
+			//randomly generates and stores a spawn position in the given range.
 			Vector3 spawnposition = new Vector3(Random.Range(-maxwidth,maxwidth),transform.position.y,0f);
+		
+			//stores spawn rotation.
 			Quaternion spawnrotation = Quaternion.identity;
 
+			//spawns the candies at the given spawn position and rotation.
 			Instantiate(candies[Random.Range(0,candies.Length)],spawnposition,spawnrotation);
 			
+			// time gap between successive spawns.
 			yield return new WaitForSeconds(Random.Range(0.8f,1.6f));
 		}
 		
+		//checks if the highscore is broken.
 		highscore.compare();
 		
+		//displays restart and MainMenu button after 2 seconds.
 		yield return new WaitForSeconds(2f);
 		
 		restart.SetActive(true);
