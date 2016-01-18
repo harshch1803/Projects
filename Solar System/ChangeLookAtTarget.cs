f4145058b263ce6c59ca42a52c7b46639970a747
harshch1803 <harshch1803@gmail.com>
2016-01-18 22:27:38 +0530

    Update ChangeLookAtTarget.cs

--- Solar System/ChangeLookAtTarget.cs
+++ Solar System/ChangeLookAtTarget.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ChangeLookAtTarget : MonoBehaviour {
+public class ChangeLookAtTarget : MonoBehaviour
+{
 
 	public GameObject target; // the target that the camera should look at
 
-	void Start() {
+	void Start() 
+	{
 		if (target == null) 
 		{
 			target = this.gameObject;
 			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
 		}
 	}
+
 
 	// Called when MouseDown on this gameObject
 	void OnMouseDown () {
