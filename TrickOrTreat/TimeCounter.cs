18415182987a82fbab3b7d34ffa7f5ee5f1bffaf
harshch1803 <harshch1803@gmail.com>
2016-01-18 19:15:15 +0530

    Update TimeCounter.cs

--- TrickOrTreat/TimeCounter.cs
+++ TrickOrTreat/TimeCounter.cs
@@ -2,26 +2,30 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class TimeCounter : MonoBehaviour {
+public class TimeCounter : MonoBehaviour
+{
 
 	public Text time;
 	public float timer;
 	public bool playing=false;
+	
 	// Update is called once per frame
 	void FixedUpdate() 
 	{
 		if(playing)
 		{
-		if(timer<0)
-		{
-			timer=0;
-			playing = false;
-		}
-		timer -=Time.deltaTime;
-		timecountdown();
+			if(timer<0)
+			{
+				timer=0;
+				playing = false;
+			}
+			
+			timer -=Time.deltaTime;
+			timecountdown();
 		}
 	
 	}
+	
 	void timecountdown()
 	{
 		time.text = "X"+Mathf.RoundToInt(timer);
