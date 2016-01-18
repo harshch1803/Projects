c56f0613ff534793197b9abb6a92485a829f61ec
harshch1803 <harshch1803@gmail.com>
2016-01-18 19:08:39 +0530

    Update GameController.cs

--- TrickOrTreat/GameController.cs
+++ TrickOrTreat/GameController.cs
@@ -8,24 +8,32 @@ public class GameController : MonoBehaviour
 	public GameObject[] candies;
 	public GameObject restart,MainMenu;
 	public Text instruction;
+	
 	HighScore highscore;
 	TimeCounter timecounter;
+	
 	void Awake()
 	{
 		MainMenu.SetActive(false);
 		restart.SetActive(false);
 	}
+	
 	// Use this for initialization
 	void Start () 
 	{
 		if(cam==null)
 			cam=Camera.main;
+			
 		timecounter = gameObject.GetComponent<TimeCounter>();
+		
 		highscore = GameObject.Find("GameController").GetComponent<HighScore>();
+		
 		Vector3 screenlimit = new Vector3(Screen.width,Screen.height,0f);
 		Vector3 worldlimit = cam.ScreenToWorldPoint(screenlimit);
 		maxwidth = worldlimit.x-.9f;
+		
 		instruction.enabled=true;
+		
 		StartCoroutine("spawn");
 	
 
@@ -34,19 +42,26 @@ public class GameController : MonoBehaviour
 	IEnumerator spawn()
 	{
 		yield return new WaitForSeconds(3f);
+		
 		instruction.enabled=false;
 		timecounter.playing=true;
+		
 		yield return new WaitForSeconds(2f);
+		
 		while(timecounter.playing)
 		{
-		Vector3 spawnposition = new Vector3(Random.Range(-maxwidth,maxwidth),transform.position.y,0f);
-		Quaternion spawnrotation = Quaternion.identity;
+			Vector3 spawnposition = new Vector3(Random.Range(-maxwidth,maxwidth),transform.position.y,0f);
+			Quaternion spawnrotation = Quaternion.identity;
 
 			Instantiate(candies[Random.Range(0,candies.Length)],spawnposition,spawnrotation);
-		yield return new WaitForSeconds(Random.Range(0.8f,1.6f));
+			
+			yield return new WaitForSeconds(Random.Range(0.8f,1.6f));
 		}
+		
 		highscore.compare();
+		
 		yield return new WaitForSeconds(2f);
+		
 		restart.SetActive(true);
 		MainMenu.SetActive(true);
 	}
